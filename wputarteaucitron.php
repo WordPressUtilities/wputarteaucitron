621397ec0c2a7156cbef2326e023b934255fa64e
Kévin <kevin@rocher-dev.com>
2026-09-15 13:32:21 +0200

    v 1.6.1
    Fix github actions

--- wputarteaucitron.php
+++ wputarteaucitron.php
@@ -5,7 +5,7 @@ Plugin Name: WPU Tarte Au Citron
 Plugin URI: https://github.com/WordPressUtilities/wputarteaucitron
 Update URI: https://github.com/WordPressUtilities/wputarteaucitron
 Description: Simple implementation for Tarteaucitron.js
-Version: 1.6.0
+Version: 1.6.1
 Author: Darklg
 Author URI: https://darklg.me/
 Text Domain: wputarteaucitron
@@ -22,7 +22,7 @@ class WPUTarteAuCitron {
     public $plugin_description;
     public $settings_details;
     public $settings;
-    private $plugin_version = '1.6.0';
+    private $plugin_version = '1.6.1';
     private $tarteaucitron_version = '1.34.0';
     private $settings_obj;
     private $stats_obj = false;
